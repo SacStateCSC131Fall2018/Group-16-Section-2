2f35c05e3616196f913c5307b46ad61993f886dd
lynguyen <lynguyen@Ly_Manjaropc>
2018-12-01 14:17:44 -0800

    Removed FileMenu Class, Added MenuBar Class.

--- PirexProject/gui_components/FileMenu.java
+++
@@ -1,5 +0,0 @@
-package gui_components;
-
-public class FileMenu {
-
-}
---
+++ PirexProject/gui_components/MenuBar.java
@@ -0,0 +1,42 @@
+import javax.swing.JPanel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+/*/
+ * Authors:
+ * 		Ly Nguyen
+ * Team: Group 16 Section 2
+ */
+public class MenuBar extends JMenuBar {
+	JMenu fileMenu, optionsMenu, helpMenu;
+	JMenuItem item;
+
+	public MenuBar() {
+		super();			//Intiallize Bar
+		setupMenus();
+	}
+	
+	
+	public void setupMenus() {
+		//FileMenu set up by Ly
+		fileMenu = new JMenu("File");
+		item = new JMenuItem("Save Query");
+		fileMenu.add(item);
+		item = new JMenuItem("Load Query");
+		fileMenu.add(item);
+		item = new JMenuItem("Import");
+		fileMenu.add(item);
+		item = new JMenuItem("Export");
+		fileMenu.add(item);
+		item = new JMenuItem("Save Query");
+		fileMenu.add(item);
+		item = new JMenuItem("Exit");
+		fileMenu.add(item);
+		this.add(fileMenu);
+		
+		//Help Menu
+		
+		//Options Menu
+	}
+	
+}
